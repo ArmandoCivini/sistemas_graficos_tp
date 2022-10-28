5de7f627b32fba866e6286c87a4cbd5d8c283ff5
Tomas Civini <tcivini@gmail.com>
2022-10-28 12:24:51 -0300

    added cameras

--- droneCamera.js
+++ droneCamera.js
@@ -1,133 +1,124 @@
 
-    function DroneCameraControl(initialPos){
+function DroneCameraControl(initialPos, app){
+    let angle = 0;
 
-        let MIN_Y=1;
+    let MIN_Y=1;
 
-        let DELTA_TRASLACION=0.1;        
-        let DELTA_ROTACION=0.02;        
-        let FACTOR_INERCIA=0.05;
+    let DELTA_TRASLACION=0.1;        
+    let DELTA_ROTACION=0.02;        
+    let FACTOR_INERCIA=0.05;
 
-        let vec3=glMatrix.vec3;  //defino vec3 para no tener que escribir glMatrix.vec3
-        let mat4=glMatrix.mat4;
+    let vec3=glMatrix.vec3;  //defino vec3 para no tener que escribir glMatrix.vec3
+    let mat4=glMatrix.mat4;
 
-        if (!initialPos) initialPos=[0,0,0];
+    if (!initialPos) initialPos=[0,0,0];
 
-        let position=vec3.fromValues(initialPos[0],initialPos[1],initialPos[2]);
-        let rotation=vec3.create();
+    let position=vec3.fromValues(initialPos[0],initialPos[1],initialPos[2]);
+    let rotation=vec3.create();
 
-        let worldMatrix=mat4.create();
+    let worldMatrix=mat4.create();
 
-        let camInitialState={
-            xVel:0,
-            zVel:0,
-            yVel:0,
-            xVelTarget:0,
-            zVelTarget:0,
-            yVelTarget:0,
+    let camInitialState={
+        xVel:0,
+        zVel:0,
+        yVel:0,
+        xVelTarget:0,
+        zVelTarget:0,
+        yVelTarget:0,
 
-            yRotVelTarget:0,
-            yRotVel:0,
-            zRotVelTarget:0,
-            zRotVel:0,
-            xRotVelTarget:0,
-            xRotVel:0,
-            
-            rightAxisMode:"move"
-        }
-
-        let camState=Object.assign({},camInitialState);
-
+        yRotVelTarget:0,
+        yRotVel:0,
+        zRotVelTarget:0,
+        zRotVel:0,
+        xRotVelTarget:0,
+        xRotVel:0,
         
-        // Eventos de teclado **********************************************
+        angle:Math.PI/2,
+        angle_change:0,
+        rightAxisMode:"move"
+    }
 
-        document.addEventListener("keydown",function(e){
-            //console.log(e.key);
-                
-            /*
-                ASDW para rotar en 2 ejes
+    let camState=Object.assign({},camInitialState);
 
-                Flechas + PgUp/PgDw o HJKUOL para trasladarse en XYZ
-
-            */
-
+    var mouseIsDown = false;
+    // Eventos de teclado **********************************************
+    document.addEventListener("keydown",function(e){
+        //console.log(e.key);
+        if (app.view == "libre") {
             switch ( e.key ) {
 
-                case "ArrowUp":  case "u": // up
+                case "ArrowUp":  case "w": // up
                     camState.zVelTarget=DELTA_TRASLACION; break;
-                case "ArrowDown": case "j": // down
+                case "ArrowDown": case "s": // down
                     camState.zVelTarget=-DELTA_TRASLACION; break; 
 
-                case "ArrowLeft": case "h": // left
+                case "ArrowLeft": case "a": // left
                     camState.xVelTarget=DELTA_TRASLACION;break;
-                case "ArrowRight": case "k": // right
-                    camState.xVelTarget=-DELTA_TRASLACION; break;   
-
-                case "w": case "o": // PgUp
-                    camState.yVelTarget=DELTA_TRASLACION;break;
-                case "s": case "l": // PgDw
-                    camState.yVelTarget=-DELTA_TRASLACION; break;        
-   
+                case "ArrowRight": case "d": // right
+                    camState.xVelTarget=-DELTA_TRASLACION; break;          
+                    
                 case "a": 
                     camState.yRotVelTarget=DELTA_ROTACION; break;                
                 case "d": 
                     camState.yRotVelTarget=-DELTA_ROTACION; break;
-         /*           
+                //rot x
                 case "s":
                     camState.xRotVelTarget=-DELTA_ROTACION;break;                                 
                 case "w": 
-                    camState.xRotVelTarget=DELTA_ROTACION;break;
-           */     
+                    camState.xRotVelTarget=DELTA_ROTACION;break;                 
 
+            }
+        } else {
+            switch ( e.key ) {
+                case "ArrowLeft": case "a":
+                    camState.angle_change = -0.05; break;
+                case "ArrowRight": case "d":
+                    camState.angle_change = 0.05; break;
+            }
+        }            
 
-                        
-                case "r": 
-                    rotation=vec3.create();
-                    position=vec3.fromValues(initialPos[0],initialPos[1],initialPos[2]);
-                    camState=Object.assign({},camInitialState);
-                    break;
+    })
 
-                case "t": 
-                    rotation=vec3.create();                    
-                    camState=Object.assign({},camInitialState);
-                    break;                    
-
-            }               
-
-        })
-
-        document.addEventListener("keyup",function(e){
-
-            switch ( e.key ) 
-            {
-                case "ArrowUp":  case "u": case "ArrowDown": case "j": 
+    document.addEventListener("keyup",function(e){
+        if (app.view == "libre") {
+            switch ( e.key ) {
+                case "ArrowUp":  case "w": case "ArrowDown": case "s": 
                     camState.zVelTarget=0; break;
                 
-                case "ArrowLeft": case "h": case "ArrowRight": case "k": 
-                    camState.xVelTarget=0; break;  
+                case "ArrowLeft": case "a": case "ArrowRight": case "d": 
+                    camState.xVelTarget=0; break;                         
+            }            
+        } else {
+            switch ( e.key ) {
+                case "ArrowLeft": case "a": case "ArrowRight": case "d": 
+                    camState.angle_change = 0; break;                         
+            }
+        }
+    })
 
-                case "w": case "o": case "s": case "l":
-                    camState.yVelTarget=0;break;
+    document.addEventListener("mousemove", function(e) {
+        if (app.view != "libre") return;
+        if (!mouseIsDown) return;
+        console.log(e.movementY);
+        camState.xRotVelTarget=e.movementY/100;
+        camState.yRotVelTarget=e.movementX/100;
+    });
+
+    document.addEventListener("mousedown", function(e) {
+        if (app.view != "libre") return;
+        mouseIsDown = true;
+    });
+
+    document.addEventListener("mouseup", function(e) {
+        if (app.view != "libre") return;
+        mouseIsDown = false;
+        camState.xRotVelTarget=0;
+        camState.yRotVelTarget=0;
+    });
     
-  
-                case "a": 
-                    camState.yRotVelTarget=0; break;
-                case "d": 
-                    camState.yRotVelTarget=0; break;
-                    /*
-                case "w": 
-                    camState.xRotVelTarget=0;break;  
 
-                case "s":
-                    camState.xRotVelTarget=0;break;                           
-                    */
-                
-            }                 
-            
-        })
-        
-
-        this.update=function(){
-            
+    this.update=function(){
+        if (app.view == "libre") {
             camState.xVel+=(camState.xVelTarget-camState.xVel)*FACTOR_INERCIA;
             camState.yVel+=(camState.yVelTarget-camState.yVel)*FACTOR_INERCIA;
             camState.zVel+=(camState.zVelTarget-camState.zVel)*FACTOR_INERCIA;
@@ -143,13 +134,9 @@
             vec3.add(rotation,rotation,rotIncrement);
 
             rotation[0]=Math.min(Math.PI/8,Math.max(-Math.PI/8,rotation[0]));
-			
-            	
 
             let rotationMatrix=mat4.create();		
-			
-
-
+            
             mat4.rotateX(rotationMatrix,rotationMatrix,rotation[0]);
 
             
@@ -159,14 +146,6 @@
             vec3.transformMat4(yAxis,yAxis,xRotation);
 
             mat4.rotate(rotationMatrix,rotationMatrix,rotation[1],yAxis);
-            
-
-
-            //mat4.rotateY(rotationMatrix,rotationMatrix,rotation[1]);
-            //mat4.rotateZ(rotationMatrix,rotationMatrix,rotation[2]);
-
-
-
 
             vec3.transformMat4(translation,translation,rotationMatrix);
             vec3.add(position,position,translation);
@@ -175,23 +154,45 @@
             mat4.translate(worldMatrix,worldMatrix,position);        
             mat4.multiply(worldMatrix,worldMatrix,rotationMatrix);
             
-            
+            camState.xRotVelTarget=0;
+            camState.yRotVelTarget=0;
+        } else {
+            camState.angle += camState.angle_change;
+            let eye = vec3.fromValues(-Math.cos(camState.angle), -1, Math.sin(camState.angle));
+            let center = vec3.fromValues(0, -1, 0);
+            let up_direction = vec3.fromValues(0, 1, 0);
+
+            let lookAtMatrix=mat4.create();
+            mat4.lookAt(lookAtMatrix, eye, center, up_direction);
+            worldMatrix=mat4.create();
+            console.log(camState.angle);
+            position = vec3.fromValues(7 * Math.cos(camState.angle), 0, 7 * Math.sin(camState.angle)+0.5);
+            if (app.view == "catapulta") {
+                position = vec3.fromValues(3.9 * Math.cos(camState.angle)+4.49, 0, 3.9 * Math.sin(camState.angle)+4.78);
+            }
+            mat4.translate(worldMatrix,worldMatrix,position);        
+            mat4.multiply(worldMatrix,worldMatrix,lookAtMatrix);
         }
+    }
 
 
-        this.getViewMatrix=function(){
+    this.getViewMatrix=function(){
 
-            let m=mat4.clone(worldMatrix);            
-            mat4.invert(m,m);
-            return m;
-        }
+        let m=mat4.clone(worldMatrix);            
+        mat4.invert(m,m);
+        return m;
+    }
 
-        this.getMatrix=function(){
+    this.getMatrix=function(){
 
-            return worldMatrix;
-
-        }
-
-
+        return worldMatrix;
 
     }
+
+    this.changeCastleMode=function() {
+        console.log("modo castillo");
+        rotation=vec3.create();
+        position=vec3.fromValues(initialPos[0],initialPos[1],initialPos[2]);
+        camState=Object.assign({},camInitialState);
+    }
+}
